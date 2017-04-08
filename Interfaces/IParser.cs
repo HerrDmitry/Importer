86ba98df3ad52fb2a9de80b69eb0072ce959ce28
HerrDmitry <HerrDmitry@gmail.com>
2017-04-08 13:10:36 -0400

    more files

---
+++ Interfaces/IParser.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace TestConsoleApp.Interfaces
+{
+    public interface IParser
+    {
+        string Parse(string input);
+    }
+}
---
+++ Interfaces/IPipe.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace TestConsoleApp.Interfaces
+{
+	public interface IPipe
+	{
+        void AddParser(IParser parser);
+
+	}
+}
