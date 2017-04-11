ae2cf346e3cd5f154f54b4bc02ce27991ed5e0c2
HerrDmitry <herrdmitry@gmail.com>
2017-04-11 18:17:04 -0400

    updates

---
+++ Implementations/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Importer.Interfaces;
+
+namespace Importer.Implementations
+{
+    public class Configuration
+    {
+        public void Load(string filePath)
+        {
+        }
+
+        public IReader
+    }
+}
