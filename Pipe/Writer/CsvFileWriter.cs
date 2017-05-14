48bb0e03dc68bbed3f1398018b3192452890edff
HerrDmitry <HerrDmitry@gmail.com>
2017-05-14 18:38:12 -0400

    updates

--- Pipe/Writer/CsvFileWriter.cs
+++ Pipe/Writer/CsvFileWriter.cs
@@ -1,15 +1,34 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Importer.Pipe.Configuration;
 
 namespace Importer.Pipe.Writer
 {
-    public class CsvFileWriter
+    public class CsvFileWriter:IDisposable
     {
         public CsvFileWriter(Stream target, CsvFileConfiguration config)
         {
             this.config = config;
         }
 
+        public Write()
+
         private CsvFileConfiguration config;
+        private StreamWriter writer;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.writer?.Dispose();
+                this.writer = null;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
     }
 }
